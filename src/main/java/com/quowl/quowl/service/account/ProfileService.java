9a17f1d2296637a23e40261e2f672851c729c102
nllsdfx <nllsdfx@gmail.com>
2016-03-11 19:31:39 +0300

    Seems can send profileBean now.

--- src/main/java/com/quowl/quowl/service/account/ProfileService.java
+++ src/main/java/com/quowl/quowl/service/account/ProfileService.java
@@ -1,5 +1,7 @@
 package com.quowl.quowl.service.account;
 
+import com.quowl.quowl.domain.logic.user.ProfileInfo;
+import com.quowl.quowl.repository.user.ProfileRepository;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +11,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
-public class ProfileService {
+public class ProfileService
+        implements com.quowl.quowl.web.beans.Service<ProfileInfo, Long> {
+
+    @Inject
+    private ProfileRepository profileRepository;
 
     public List<Long> parseFromString(String str) {
         String stringWithoutBrackets = StringUtils.substringBetween(str, "[", "]");
@@ -18,5 +24,39 @@ public class ProfileService {
 
         return result;
     }
+
+    @Override
+    public void save(ProfileInfo object) {
+        profileRepository.save(object);
+    }
+
+    @Override
+    public void delete(ProfileInfo object) {
+        profileRepository.delete(object);
+    }
+
+    @Override
+    public void delete(Long aLong) {
+        profileRepository.delete(aLong);
+    }
+
+    @Override
+    public List<ProfileInfo> findAll() {
+
+        return profileRepository.findAll();
+    }
+
+    @Override
+    public ProfileInfo findOne(Long aLong) {
+        return profileRepository.findOne(aLong);
+    }
+
+    @Override
+    public boolean exists(Long aLong) {
+        return profileRepository.exists(aLong);
+    }
+
+
+
 
 }
--- src/main/java/com/quowl/quowl/web/beans/user/ProfileBean.java
+++ src/main/java/com/quowl/quowl/web/beans/user/ProfileBean.java
@@ -1,16 +1,23 @@
 package com.quowl.quowl.web.beans.user;
 
 
+import com.quowl.quowl.domain.logic.user.Gender;
 import com.quowl.quowl.domain.logic.user.ProfileInfo;
+import org.joda.time.LocalDate;
+
 
 public class ProfileBean {
 
     private String firstName;
     private String lastName;
+    private Gender gender;
+    private LocalDate birthday;
 
     public ProfileBean(ProfileInfo profileInfo) {
         this.firstName = profileInfo.getFirstName();
         this.lastName = profileInfo.getLastName();
+        this.gender = profileInfo.getGender();
+        this.birthday = profileInfo.getBirthDate();
     }
 
     public String getFirstName() {
@@ -27,5 +34,21 @@ public class ProfileBean {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
     }
 }
