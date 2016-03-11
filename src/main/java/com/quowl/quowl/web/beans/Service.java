277239d68d33994cdc5bd3b53144e3539fbb8f82
nllsdfx <nllsdfx@gmail.com>
2016-03-11 18:42:23 +0300

    Service interface for all services

---
+++ src/main/java/com/quowl/quowl/web/beans/Service.java
@@ -0,0 +1,62 @@
+package com.quowl.quowl.web.beans;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+/**
+ * Interface for generic CRUD operations on a service for
+ * a specific type.
+ *
+ * @author nllsdfx
+ */
+public interface Service<T, ID extends Serializable> {
+
+    /**
+     * Saves the given object.
+     *
+     * @param object the entity to save.
+     */
+    void save(T object);
+
+    /**
+     * Deletes the given object.
+     *
+     * @param object the entity to delete.
+     */
+    void delete(T object);
+
+    /**
+     * Deletes the entity with given id.
+     *
+     * @param id must not be null.
+     */
+    void delete(ID id);
+
+
+    /**
+     * Finds all entities.
+     *
+     * @return list of founded entities.
+     */
+    List<T> findAll();
+
+    /**
+     * Find entity by given id.
+     *
+     * @param id must not be null.
+     * @return the entity with the given id.
+     */
+    T findOne(ID id);
+
+    /**
+     * Checks whether entity with the given id exists.
+     *
+     * @param id must not be null.
+     * @return <code>true</code> if entity exists,
+     * <code>false</code> otherwise.
+     */
+    boolean exists(ID id);
+}
+
+
