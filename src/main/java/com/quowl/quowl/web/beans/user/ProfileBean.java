3199bdae9b8e5c497001fe1c452185bb5aaef70d
nllsdfx <nllsdfx@gmail.com>
2016-03-11 21:09:08 +0300

    settings logic added

--- src/main/java/com/quowl/quowl/web/beans/user/ProfileBean.java
+++ src/main/java/com/quowl/quowl/web/beans/user/ProfileBean.java
@@ -3,21 +3,26 @@ package com.quowl.quowl.web.beans.user;
 
 import com.quowl.quowl.domain.logic.user.Gender;
 import com.quowl.quowl.domain.logic.user.ProfileInfo;
+import com.quowl.quowl.web.beans.WebBean;
 import org.joda.time.LocalDate;
 
 
-public class ProfileBean {
+public class ProfileBean implements WebBean<ProfileInfo> {
 
+    private long id;
     private String firstName;
     private String lastName;
     private Gender gender;
     private LocalDate birthday;
+
+    public ProfileBean(){}
 
     public ProfileBean(ProfileInfo profileInfo) {
         this.firstName = profileInfo.getFirstName();
         this.lastName = profileInfo.getLastName();
         this.gender = profileInfo.getGender();
         this.birthday = profileInfo.getBirthDate();
+        this.id = profileInfo.getId();
     }
 
     public String getFirstName() {
@@ -50,5 +55,27 @@ public class ProfileBean {
 
     public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public void copyDataFromDomain(ProfileInfo obj) {
+
+    }
+
+    @Override
+    public void copyDataToDomain(ProfileInfo obj) {
+        obj.setId(id);
+        obj.setBirthDate(birthday);
+        obj.setFirstName(firstName);
+        obj.setGender(gender);
+        obj.setLastName(lastName);
     }
 }
