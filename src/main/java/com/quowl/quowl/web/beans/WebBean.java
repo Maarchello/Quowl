8c539df32710ecc51e3ecee38b2ea2d24fedce35
nllsdfx <nllsdfx@gmail.com>
2016-03-12 13:30:35 +0300

    javadocs

--- src/main/java/com/quowl/quowl/web/beans/WebBean.java
+++ src/main/java/com/quowl/quowl/web/beans/WebBean.java
@@ -1,10 +1,26 @@
 package com.quowl.quowl.web.beans;
 
-
+/**
+ * Interface should be implemented by any
+ * classes that are WebBeans or DTO objects.
+ *
+ * @param <D> the type of entity.
+ */
 public interface WebBean<D> {
 
+    /**
+     * Copies all data from the given entity
+     * to DTO object.
+     *
+     * @param obj the entity object to copy from.
+     */
     void copyDataFromDomain(D obj);
 
+    /**
+     * Copies all data to the given entity.
+     *
+     * @param obj the entity object to copy in.
+     */
     void copyDataToDomain(D obj);
 
 
