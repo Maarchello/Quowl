77dc0812f84910dc2c5d3357a4d53d82c72cf962
nllsdfx <nllsdfx@gmail.com>
2016-03-30 14:06:22 +0300

    refactor

--- src/main/webapp/resources/js/design/index-design.js
+++ src/main/webapp/resources/js/design/index-design.js
@@ -1,5 +1,18 @@
+/**
+ * Get started button
+ */
 $(".button-fill").hover(function () {
     $(this).children(".button-inside").addClass('full');
 }, function () {
     $(this).children(".button-inside").removeClass('full');
+});
+/**
+ * Login modal
+ */
+$('.toggle').on('click', function() {
+    $('.login-container').stop().addClass('active');
+});
+
+$('.close').on('click', function() {
+    $('.login-container').stop().removeClass('active');
 });
--- src/main/webapp/resources/js/index.js
+++
@@ -1,7 +0,0 @@
-$('.toggle').on('click', function() {
-  $('.login-container').stop().addClass('active');
-});
-
-$('.close').on('click', function() {
-  $('.login-container').stop().removeClass('active');
-});
